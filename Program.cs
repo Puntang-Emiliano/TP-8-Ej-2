87b125024422a0320b8163b86cca1bbc1d0e650f
Puntang Emiliano <Puntang.emiliano@gmail.com>
2022-11-10 18:50:25 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ej_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        string[] diasSemana= {"lunes", "martes", "miercoles", "jueves", "viernes"};
+        int dias=5;
+          for (int i=dias-1 ;i >=0; i-- )
+          {
+          Console.WriteLine( "dia: " + diasSemana[i]);
+          }
+
+        Console.WriteLine("presiones una tecla para terminar...");
+        Console.ReadKey();
+
+           
+        }
+    }
+}
